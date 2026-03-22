8259d350140e04692e4b4e588d6797966ec42552
Mounir <izem.mounir@gmail.com>
2026-03-22 14:24:18 +0100

    feat: add cron job for LBMA fixings AM and PM

---
+++ backend/src/crons/spotCron.js
@@ -0,0 +1,22 @@
+const cron = require('node-cron');
+const {saveSpotPrice} = require('../services/spotService.js');
+
+cron.schedule('30 10 * * *', async () => {
+    try {
+        await saveSpotPrice()
+    } catch (error) {
+        console.error(error)
+    }
+}, {
+    timezone: 'Europe/London'
+})
+
+cron.schedule('00 15 * * *', async () => {
+    try {
+        await saveSpotPrice()
+    } catch (error) {
+        console.error(error)
+    }
+}, {
+    timezone: 'Europe/London'
+})
